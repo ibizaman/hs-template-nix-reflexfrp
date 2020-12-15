ec8b0971770edb72e23b6b282f63e453e133ab23
ibzaman <ibizapeanut@gmail.com>
2020-12-14 23:57:59 -0800

    add building backend with stack

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
 .PHONY: build-release-backend
 .PHONY: run-release-backend
+.PHONY: build-backend
+.PHONY: run-backend
 
 build-release-backend:
 	nix-build --pure -o backend-result -A ghc.backend
 
 run-release-backend:
 	./backend-result/bin/backend-exe
+
+build-backend:
+	nix-shell --pure --run 'stack build backend'
+
+run-backend:
+	nix-shell --pure --run 'stack run backend'
--- release.nix
+++ release.nix
@@ -1,7 +1,7 @@
 { compiler ? "ghc865"
 , system ? builtins.currentSystem
 , reflex-platform ? fetchTarball "https://github.com/reflex-frp/reflex-platform/archive/v0.6.2.0.tar.gz"
-, pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz") {}
+, pkgs' ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz") {}
 }:
 (import reflex-platform { inherit system; }).project (
   { pkgs, ... }: {
@@ -15,11 +15,32 @@
       ghc = ["backend"];
     };
 
-    shellToolOverrides = self: super:
+    shellToolOverrides = _ghc: super:
       let
-        inherit (pkgs.haskell.lib) dontCheck;
-      in {
+        inherit (pkgs'.haskell.lib) dontCheck;
+
+        ghc = pkgs'.haskell.packages.${compiler};
+      in with pkgs'; rec {
         haskell-ide-engine = null;
+
+        stack = dontCheck ((ghc.callHackage "stack" "2.3.1" {}).override rec {
+          Cabal = dontCheck (ghc.callHackage "Cabal" "3.0.2.0" {});
+          hpack = dontCheck (ghc.hpack.override {
+            inherit Cabal;
+          });
+          rio-prettyprint = dontCheck (ghc.rio-prettyprint.override {
+            inherit Cabal;
+          });
+          hackage-security = dontCheck (ghc.hackage-security.override {
+            inherit Cabal;
+          });
+          http-download = dontCheck (ghc.http-download.override {
+            inherit rio-prettyprint;
+          });
+          pantry = dontCheck (ghc.pantry.override {
+            inherit Cabal hackage-security hpack http-download rio-prettyprint;
+          });
+        });
       };
 
     overrides = self: super: {
